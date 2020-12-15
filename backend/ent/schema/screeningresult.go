ee7002b0f72fa290b28a55285df8e2217a51451a
FICKKM <adab.writer@gmail.com>
2020-12-15 19:22:22 +0700

    ทำ entity สำหรับเก็บข้อมูลผลการคัดกรอง - close #22

---
+++ backend/ent/schema/screeningresult.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// ScreeningResult holds the schema definition for the ScreeningResult entity.
+type ScreeningResult struct {
+	ent.Schema
+}
+
+// Fields of the ScreeningResult.
+func (ScreeningResult) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the ScreeningResult.
+func (ScreeningResult) Edges() []ent.Edge {
+	return nil
+}
