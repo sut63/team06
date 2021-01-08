20690e91c08f4234c8a7239a87026e45e9d0c7ed
MiNORVAR <kuchitzkuchita@gmail.com>
2021-01-09 00:11:50 +0700

    สร้าง Entity output หลักและ Entity สนับสนุนสำหรับระบบลงทะเบียนผู้ป่วย - close #59

---
+++ backend/ent/schema/gender.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Gender holds the schema definition for the Gender entity.
+type Gender struct {
+	ent.Schema
+}
+
+// Fields of the Gender.
+func (Gender) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("gender").NotEmpty(),
+	}
+}
+
+// Edges of the Gender.
+func (Gender) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("GenderToPatient", Patient.Type),
+	}
+}
---
+++ backend/ent/schema/patient.go
@@ -0,0 +1,37 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Patient holds the schema definition for the Patient entity.
+type Patient struct {
+	ent.Schema
+}
+
+// Fields of the Patient.
+func (Patient) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("personalID").Positive().Unique(),
+		field.String("patientName").NotEmpty(),
+		field.Int("age").Positive(),
+		field.String("hospitalNumber").NotEmpty().Unique(),
+		field.String("drugAllergy").NotEmpty(),
+		field.Time("added_time"),
+	}
+}
+
+// Edges of the Patient.
+func (Patient) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("Prefix", Prefix.Type).Ref("PrefixToPatient"),
+		edge.From("Gender", Gender.Type).Ref("GenderToPatient"),
+		edge.From("Bloodtype", BloodType.Type).Ref("BloodTypeToPatient"),
+		edge.To("PatientToTriageResult", TriageResult.Type),
+		edge.To("PatientToAppointmentResults", AppointmentResults.Type),
+		edge.To("PatientToMedicalProcedure", MedicalProcedure.Type),
+		edge.To("PatientToRightToTreatment", RightToTreatment.Type),
+	}
+}
