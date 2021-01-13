dc6e53b66b0507b8445f3640da8fbe5cbceee3db
iYokky <b6117368@g.sut.ac.th>
2021-01-13 15:00:04 +0700

    ทำ UI ระบบตรวรักษา - close #99

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,9 +1,13 @@
 import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
+import DiagnosisPage from './components/DiagnosisPage';
+
 
 export const plugin = createPlugin({
   id: 'welcome',
   register({ router }) {
     router.registerRoute('/welcome', WelcomePage);
+    router.registerRoute('/diagnosis', DiagnosisPage);
   },
 });
+
